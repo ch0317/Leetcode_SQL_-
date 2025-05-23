7e1e99a8623d11fc87e8c3744bb4ea404ff27d6a
chen <chenhao03@sf.local>
2025-05-23 17:37:03 +0800

    sql test

--- 178.scoreRank.sql
+++ 178.scoreRank.sql
@@ -46,3 +46,18 @@
 -- | 3.65  | 3    |
 -- | 3.50  | 4    |
 -- +-------+------+
+
+CREATE TABLE Scores (id INT PRIMARY KEY, score DECIMAL(4,2));
+
+INSERT INTO Scores (id, score) VALUES
+(1, 3.50),
+(2, 3.65),
+(3, 4.00),
+(4, 3.85),
+(5, 4.00),
+(6, 3.65);
+
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS rank
+FROM Scores;
