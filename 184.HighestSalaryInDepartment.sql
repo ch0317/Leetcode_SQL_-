a039c7e9a10ea817fea43baf973f206dfc7cb5ad
chen <chenhao03@sf.local>
2025-05-26 14:55:18 +0800

    leet sql

--- 184.HighestSalaryInDepartment.sql
+++ 184.HighestSalaryInDepartment.sql
@@ -60,3 +60,52 @@
 -- | IT         | Max      | 90000  |
 -- +------------+----------+--------+
 -- 解释：Max 和 Jim 在 IT 部门的工资都是最高的，Henry 在销售部的工资最高。
+
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    salary INT,
+    departmentId INT,
+    FOREIGN KEY (departmentId) REFERENCES Department(id)
+);
+
+INSERT INTO Employee (id, name, salary, departmentId) VALUES
+(1, 'Joe', 70000, 1),
+(2, 'Jim', 90000, 1),
+(3, 'Henry', 80000, 2),
+(4, 'Sam', 60000, 2),
+(5, 'Max', 90000, 1);
+
+CREATE TABLE Department (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+INSERT INTO Department (id, name) VALUES
+(1, 'IT'),
+(2, 'Sales');
+
+
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN
+(
+    SELECT departmentId,max(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.salary, e.departmentId) IN (
+    SELECT MAX(salary), departmentId
+    FROM Employee
+    GROUP BY departmentId
+);
